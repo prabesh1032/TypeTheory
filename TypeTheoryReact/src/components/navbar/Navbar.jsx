73d4f728ab38e4182285593d8f26035c42c4883e
Prabesh Acharya <praveshach1032@gmail.com>
2026-05-20 23:29:06 +0545

    feat: correct import case for Navlinks component in Navbar.jsx

--- TypeTheoryReact/src/components/navbar/Navbar.jsx
+++ TypeTheoryReact/src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Navlinks from "./navlinks";
+import Navlinks from "./Navlinks";
 import { FaSearch } from "react-icons/fa";
 import { Menu, X } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
